99c531144bfca10cb3a1938326ff7a2534824183
SaileePhal <saileemphal@gmail.com>
2025-12-17 13:33:05 +0530

    Update port binding for Render

--- hired-backend/app/__init__.py
+++ hired-backend/app/__init__.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/api/v1/*": {"origins": [
-        # Allow the exact origin where your frontend is running
-        "http://localhost:8080",
-        "http://127.0.0.1:8080" # Include this for maximum compatibility
-    ]}}, supports_credentials=True)
+    # CORS(app, resources={r"/api/v1/*": {"origins": [
+    #     # Allow the exact origin where your frontend is running
+    #     "http://localhost:8080",
+    #     "http://127.0.0.1:8080" # Include this for maximum compatibility
+    # ]}}, supports_credentials=True)
+    CORS(app, resources={r"/api/v1/*": {"origins": "*"}}, supports_credentials=True)
 
     from app.routes.auth_routes import auth_bp
     from app.routes.job_routes import job_bp
